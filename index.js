f064b469c4a17c56a4477e56e9e36e004e570fba
Nicolescu Alexandru <74272723+NicolescuAlexandru@users.noreply.github.com>
2020-11-18 01:47:32 +0200

    Eliminated test content

--- index.js
+++ index.js
@@ -4,35 +4,3 @@ function callName(lastName, firstName) {
 }
 
 callName("Alex", "Nicks");
-
-const gameBoard = [
-    ['0', 'x', null],
-    ['x', 'Hello', null],
-    ['x', null, '0']
-];
-
-console.log(gameBoard);
-
-const boysName = ['Alex',];
-const girlsName = ['Teodora', 'Maria', 'Andreea', 'Mara', 'Alexandra', 'Timea'];
-
-if (boysName.length === girlsName.length) {
-    console.log("There are as many name examples for both boys and girls");
-} else {
-    while (boysName.length !== girlsName.length) {
-        switch (boysName.length > girlsName.length || boysName < girlsName) {
-            case (boysName.length > girlsName.length):
-                girlsName.push('dafaultGirlName')
-                break;
-            case (boysName.length < girlsName.length):
-                boysName.push('defaultBoyName')
-                break;
-            default:
-                console.log("Something is really fishy!")
-        }
-    }
-    console.log("There are as many name examples for both boys and girls")
-}
-
-console.log(boysName);
-console.log(girlsName);
